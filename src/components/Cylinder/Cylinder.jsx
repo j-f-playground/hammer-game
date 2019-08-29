554eac439d0c572f28ebc031a6af96f0f8749756
Estefanía Salgado Alameda <estefania.salgado@gmail.com>
2019-08-29 10:21:48 +0200

    play game

--- src/components/Cylinder/Cylinder.jsx
+++ src/components/Cylinder/Cylinder.jsx
@@ -1,4 +1,4 @@
-import { bool, func, string } from 'prop-types';
+import { bool, func, string, num } from 'prop-types';
 import React from 'react';
 import styles from './styles.module.scss';
 
@@ -8,18 +8,23 @@ class Cylinder extends React.PureComponent {
     name: string.isRequired,
     isSmall: bool,
     onClick: func,
+    index: num,
   };
 
   static defaultProps = {
     isSmall: false,
     onClick: undefined,
+    index: undefined,
   };
 
-  render() {
-    const { color, isSmall, onClick } = this.props;
+  onClick = () => {
+    this.props.onClick(this.props.name, this.props.index)
+  }
 
+  render() {
+    const { color, isSmall } = this.props;
     return (
-      <div className={`${styles.container} ${isSmall ? styles.small : ''}`} onClick={onClick}>
+      <div className={`${styles.container} ${isSmall ? styles.small : ''}`} onClick={this.onClick}>
         <div className={styles.center} style={{ backgroundColor: color }} />
         <div className={styles.top} style={{ backgroundColor: color }} />
         <div className={styles.bottom} style={{ backgroundColor: color }} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,36 +10,96 @@ class Home extends React.Component {
         {
           name: 'red',
           color: '#d64a4a',
+          isSmall: false,
         },
         {
           name: 'blue',
           color: '#6663f3',
+          isSmall: false,
         },
         {
           name: 'green',
           color: '#47b96c',
+          isSmall: false,
         },
         {
           name: 'magenta',
           color: '#b848b9',
+          isSmall: false,
         },
         {
-          name: 'orange',
-          color: '#f99a06',
+          name: 'white',
+          color: '#ffffff',
+          isSmall: false,
         },
       ],
     };
   }
 
+  // handleOnClick = (name, index) => {
+  //   let newCylinders = [...this.state.cylinders]
+  //   newCylinders[index].isSmall = this.state.cylinders[index].isSmall ? false : true
+  //   if (name === 'white') {
+  //     newCylinders = newCylinders.map(item => {
+  //       let newItem = {...item};
+  //       if (item.name === 'blue' || item.name === 'red' || item.name === 'magenta' || item.name === 'green') {
+  //         newItem.isSmall = newItem.isSmall ? false : true;
+  //         //newItem.isSmall = false;
+  //       } 
+  //       return newItem;
+  //     })
+  //   }
+  //   this.setState({
+  //     cylinders: newCylinders,
+  //   });
+  // }
+
+  handleOnClick = (name, index) => {
+    let newCylinders = [...this.state.cylinders]
+    newCylinders[index].isSmall = this.state.cylinders[index].isSmall ? false : true
+
+    if (newCylinders[index].isSmall === true) {
+      newCylinders = newCylinders.map(item => {
+        let newItem = {...item};
+        newItem.isSmall = !item.isSmall ? true : false;
+        return newItem;
+      })
+    }
+
+    console.log(newCylinders);
+    
+    this.setState({
+      cylinders: newCylinders,
+    });
+  }
+
+  // handleOnClick = (name) => {
+  //   let newCylinders = [...this.state.cylinders]
+  //   newCylinders = newCylinders.map(item => {
+  //     let newItem = {...item};
+  //     newItem.isSmall = item.name === name ? !item.isSmall : false;
+  //     return newItem;
+  //   })
+
+  //   this.setState({
+  //     cylinders: newCylinders,
+  //   });
+  // }
+
   render() {
     const { cylinders } = this.state;
-
     return (
       <div className={styles.container}>
         <ul className={styles.list}>
           {cylinders.map((item, index) => (
             <li key={index}>
-              <Cylinder color={item.color} name={item.name} />
+              <Cylinder 
+                color={item.color}
+                name={item.name}
+                isSmall={item.isSmall}
+                onClick={this.handleOnClick}
+                index={index}
+              />
             </li>
           ))}
         </ul>
